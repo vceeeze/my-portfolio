b458ccc0b49ee17858abc7f725d53968af78d412
vceeeze <vceeeze11@gmail.com>
2023-09-05 11:09:45 +0100

    commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import { SocialLinks } from "./components/SocialLinks";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
+import Experience from "./components/Experience";
+import Contact from "./components/Contact";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+      <About />
+      <Portfolio />
+      <Experience />
+      <Contact />
+      <SocialLinks />
+    </div>
+  );
+}
+export default App;
---
+++ src/components/About.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div name="about" className="w-full h-screen bg-gradient-to-b from-gray-800
+    to-black text-white">
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+          About
+          </p>
+        </div>
+        <p className=" text-xl mt-20">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae
+          exercitationem explicabo incidunt asperiores provident numquam iure
+          rem molestiae facere officiis, iste commodi quasi officia fugit quas
+          in quibusdam, earum similique! Lorem ipsum dolor sit amet consectetur,
+          adipisicing elit. Quae quo, sapiente ratione, porro aspernatur
+          doloribus nam atque saepe omnis quod voluptates corporis cupiditate
+          non at illo! Praesentium officia recusandae laboriosam. </p>
+          <br />
+          <p className="text-xl">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique
+          illo obcaecati doloribus debitis? Quasi doloribus, quisquam corrupti
+          odio nesciunt error optio at minus tempore atque voluptatem, iste
+          perferendis sit ducimus quaerat maxime. Odio quisquam dolore itaque?
+          Commodi, sequi debitis odio sunt eveniet laudantium magnam quam nulla,
+          dignissimos minima, quod quae!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
